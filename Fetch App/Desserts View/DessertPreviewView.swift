bb699e91fe40d29fea29f508a15a6843439c0d4c
Sam <samhinsh@gmail.com>
2024-08-15 17:05:49 -0500

    Use DessertPreviewView with images

--- Fetch App/Desserts View/DessertPreviewView.swift
+++ Fetch App/Desserts View/DessertPreviewView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 struct DessertPreviewView: View {
     var dessert: DessertPreview
     var body: some View {
-        Text(dessert.strMeal)
+        HStack(spacing: 14) {
+            AsyncImage(url: .init(string: dessert.strMealThumb)) { phase in
+                switch phase {
+                case .empty, .failure:
+                    Color.white
+                        .frame(width: 100, height: 100)
+                case .success(let image):
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .frame(width: 100, height: 100)
+                default:
+                    Color.white
+                }
+            }
+            
+            Text(dessert.strMeal)
+                .font(.callout)
+                .fontWeight(.medium)
+        }
     }
 }
 
 #Preview {
-    DessertPreviewView(dessert: .init(strMeal: "",
-                                      strMealThumb: "",
-                                      idMeal: ""))
+    DessertPreviewView(dessert: .init(strMeal: "Apple & Blackberry Crumble",
+                                      strMealThumb: "https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg",
+                                      idMeal: "52893"))
 }

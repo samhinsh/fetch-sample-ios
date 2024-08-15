3954ae8a5d9dc823e25df47fe004da66653743ad
Sam <samhinsh@gmail.com>
2024-08-15 15:29:05 -0500

    Initial Commit

---
+++ Fetch App/Fetch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_AppApp.swift
+//  Fetch App
+//
+//  Created by Samuel Hinshelwood Jr. on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
